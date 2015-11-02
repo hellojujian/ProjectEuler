458ff31bde3873c3d90f66cbf3d8c694c81bc460
jujian <jujian@diandong.com>
2015-11-02 15:18:54 +0800

    Update p44.c

--- p44.c
+++ p44.c
@@ -2,7 +2,11 @@
  * p44.c
  * Copyright (C) 2015 jujian <jujian@diandong.com>
  *
- * Pn+1 - Pn == 3n+1
+ * 参考：
+ * http://www.mathblog.dk/project-euler-44-smallest-pair-pentagonal-numbers/
+ *
+ * 一元二次方程的根为 (-b+(b^2-4ac)^0.5)/2
+ * Pn = n(3n-1)/2  =>  n = (1+(1+24Pn)^0.5)/6
  *
  * Distributed under terms of the MIT license.
  */
@@ -10,70 +14,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
-long long int checkPentagon(long long int n);
+int checkPentagon(long long int n);
 
 int main() {
     int begin = clock();
-    long long int A[100000] = {0};
-    long long int i;
-    for (i=0; i<100000; i++) {
-        A[i] = i*(3*i-1)/2;
-    }
-    long long int a, b, c, d, flag;
-    a = 1ll;
-    b = 2ll;
-    c = 3ll;
-    d = 4ll;
-    flag = 0;
-    while (a<100000ll && b<99999ll) {
-        printf("a is %lld, b is %lld, A[a] is %lld, A[b] is %lld\n", a, b, A[a], A[b]);
-        if (A[a] + A[b] < A[b+1]) {
-            a++;
-            b = a+1ll;
-            continue;
-        } else if (!checkPentagon(A[a]+A[b])) {
-        } else if (checkPentagon(A[a]+A[b])) { 
-            printf("a is %lld, b is %lld\n", a, b);
-            printf("checkPentagon is %lld\n", checkPentagon(A[a]+A[b]));
-            if (checkPentagon(A[a]+2ll*A[b])) {
-                printf("a is %d, b is %d, c is %d\n", a, b, checkPentagon(A[a]+A[b]));
+    long long int i, j, Pi, Pj;
+    i=1;
+    int flag = 0;
+    while (!flag) {
+        i++;
+        Pi = i*(3*i-1)/2;
+        for (j=i-1; j>0; j--) {
+            Pj = j*(3*j-1)/2;
+            if (checkPentagon(Pi+Pj) && checkPentagon(Pi-Pj)) {
+                flag = 1;
                 break;
-            } else {
-                b++;
             }
         }
     }
-
-    /*
-    printf("A[8718] is %lld, A[25295] is %lld, A[25296] is %lld\n", A[8718], A[25295], A[25296]);
-    printf("%d\n", A[8718]+A[25295] < A[25296]);
-    printf("%ld\n", checkPentagon(A[8718]+A[25295]));
-    */
+    printf("i is %lld, j is %lld, D is %lld\n", i, j, i*(3*i-1)/2-j*(3*j-1)/2);
 
     int stop = clock();
     printf("time consumed is %dms\n", (stop-begin));
     return 0;
 }
 
-long long int checkPentagon(long long int n) {
-    if (n == 1ll) {
-        return 1ll;
-    }
-    long long int i, tmp, start;
-    i = 1ll;
-    start = i-1;
-    tmp = start*(3*start-1)/2;
-    while (tmp <= n) {
-        if (tmp == n) {
-            return start;
-        } else {
-            start++;
-            tmp = start*(3*start-1)/2;
-        }
-    }
-
-    return 0ll;
+int checkPentagon(long long int n) {
+    double tmp = (1+sqrt(1+24*n))/6;
+    return tmp == (int)tmp;
 }
 
 
